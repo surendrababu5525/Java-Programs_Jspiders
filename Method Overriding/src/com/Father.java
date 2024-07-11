e09a41010fa4da7a05dbe8fa353ec03fd270a3d3
surendrababu5525 <surendrababue655@gmail.com>
2024-07-11 10:57:29 +0530

    Delete Method Overriding directory

--- Method Overriding/src/com/Father.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public class Father {
-
-	void bike()
-	{
-		System.out.println("Old fashioned fathers bike");
-	}
-}
--- Method Overriding/src/com/MainClass.java
+++
@@ -1,8 +0,0 @@
-package com;
-
-public class MainClass {
-	public static void main(String[] args) {
-		Car c = new Car();
-		c.start();
-	}
-}
